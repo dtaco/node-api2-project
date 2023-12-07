98d03fcf006785db200445221407f188d46ee7b2
David Tacoronte <d.tacoronte89@gmail.com>
2023-12-07 14:48:07 -0500

    Posting section completed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -36,6 +36,29 @@ router.get('/:id', async (req,res) => {
 
 })
 router.post('/', (req,res) => {
+    const {title, contents} = req.body;
+    
+    if(!title || !contents) {
+        res.status(400).json({ 
+            message: "Please provide title and contents for the post" 
+        })
+    } else {
+        Post.insert({ title, contents })
+            .then(({ id }) => {
+                return Post.findById(id)
+            })
+            .then(newPost => {
+                res.status(201).json(newPost)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "message: There was an error while saving the post to the database",
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
+    }
+
 
 })
 router.put('/', (req,res) => {
