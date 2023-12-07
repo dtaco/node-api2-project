fd8318ac6edbef33b37dcea59dafe6b01bd22e44
David Tacoronte <d.tacoronte89@gmail.com>
2023-12-07 13:50:40 -0500

    wiring tested - g2g

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,4 +3,24 @@ const express = require('express')
 
 const router = express.Router()
 
+router.get('/', (req,res) => {
+    res.json('foo')
+})
+router.get('/', (req,res) => {
+
+})
+router.post('/', (req,res) => {
+
+})
+router.put('/', (req,res) => {
+
+})
+router.delete('/:id', (req,res) => {
+
+})
+router.get('/:id/comments', (req,res) => {
+
+})
+
+
 module.exports = router
